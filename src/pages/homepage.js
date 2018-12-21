bf4607022505e6ab143b96a769a1617d94f2af3e
Ameka Brown <brownameka05@gmail.com>
2018-12-20 22:36:35 -0600

    style added

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ProductListing from '../features/product-listing'
 import data from '../data/products.json'
+import HouReq from './HouReq.png'
 
 export default function Homepage(props){
-  return <div>
-  <h2> Homepage </h2>
+  return <div className="home">
+  <img src={HouReq} height="200"/>
+  <h1> Welcome To Houston Request </h1>
   <ProductListing products={data.products} />
   </div>
 }
