48512d9c5a6bb7781466cbf5ebf3f9fc2b9e000c
Ameka Brown <brownameka05@gmail.com>
2019-03-08 23:37:10 -0600

    update

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
         <Navigation />
         <Router />
 
-          <h1>Donate To Houston Request Please anything ! </h1>
+          <h1>Donate To Houston the Request Please anything ! </h1>
           <Elements>
             <CheckoutForm />
           </Elements>
